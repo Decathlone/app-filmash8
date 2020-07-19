7284130458310c2cc4a7827579132a41f4e71cbb
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-07-19 15:21:32 +0000

    remove extra comma

--- gtests/TestPnLs.cpp
+++ gtests/TestPnLs.cpp
@@ -132,4 +132,5 @@ TEST( VolatilityBarRollBuffer, main ) {
     EXPECT_FALSE( lbuf.isFill() );
     
     for (size_t i = 0; i < 4; i++) {
-        EXPECT_FALSE( lbuf.add( TSimpleBar{gStartingTime+ToDouble(i),ToDouble(i)*2+1.0,ToDouble(i)*2+1
+        EXPECT_FALSE( lbuf.add( TSimpleBar{gStartingTime+ToDouble(i),ToDouble(i)*2+1.0,ToDouble(i)*2+1.0,ToDouble(i),ToDouble(i),1.0} ) );
+        EXPECT_TRUE( std::isnan(lbuf.getE
