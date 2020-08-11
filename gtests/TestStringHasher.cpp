1748f518629cfeeb6522803cef8f0a219f7af92c
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-08-11 10:53:06 +0000

    v3.0.1

--- gtests/TestStringHasher.cpp
+++ gtests/TestStringHasher.cpp
@@ -10,4 +10,5 @@ TEST( StringHasher, testCounter ) {
     
     size_t lIdx = 0;
     for( size_t i=0; i<100; ++i ){
-        EXPECT_NE( GetSymbolByIdx( lIdx++ 
+        EXPECT_NE( GetSymbolByIdx( lIdx++ ) , '_' );
+        if( lIdx >= GetSymbolsCount() ) { lIdx
