fb70ac992a42be874766ff5a102f4bfd8e46deb0
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-08-22 22:27:42 +0000

    split function

--- gtests/main.cpp
+++ gtests/main.cpp
@@ -1,4 +1,7 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc,
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret = RUN_ALL_TESTS();
+    return ret;
+}
