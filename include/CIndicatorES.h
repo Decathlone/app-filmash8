e54caf507eb8061f9c10a8fb9691295c50c1b770
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-09-12 08:45:14 +0000

    hwmon: (lm85) Fix various errors on attribute writes

--- include/CIndicatorES.h
+++ include/CIndicatorES.h
@@ -22,3 +22,26 @@ struct TForecastModelParams{
         S = aS;
         t = 0.0;
         Alpha = 0.5;
+        Gamma = 0.5;
+        Phi = 0.5;    
+    }
+};
+
+//-----------------------------------------------------------------------------------
+// Class CIndicatorES
+//-----------------------------------------------------------------------------------
+class CIndicatorES : public PowellsMethod {
+    protected:
+        TDoubles Dat;                                              // Input data
+        size_t Dlen;                                               // Data length
+        TDoubles Par;                                             // Parameters
+    public:
+        CIndicatorES(): PowellsMethod(), Par( 5.0 ) { }
+        bool CalcPar( const TDoubles &dat, const TForecastModelParams &aParams );
+        double GetPar( const int n );
+        
+    private:
+        double func( const TDoubles &p ) override;
+};
+
+#endif /* CINDICATORES_H */
---
+++ include/Comparers.h
@@ -0,0 +1,3 @@
+/**
+ * \file Comparers.h
+ * \brief М
