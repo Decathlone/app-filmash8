b59aade08289d6ad6e82f08a9b1eae4df64323a1
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-09-12 12:28:06 +0000

    intelli_plug: tweak mp_decsion parameters and remove unused logic

--- include/Comparers.h
+++ include/Comparers.h
@@ -1,4 +1,6 @@
 /**
  * \file Comparers.h
  * \brief Модуль содержащий методы для сравнения чисел с заданной точностью
- * \au
+ * \author kan <kansoftware.ru>
+ * \since 2015-08-25
+ * \date 2020-02-28
