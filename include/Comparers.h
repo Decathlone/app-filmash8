3b0bc493f63d565b2442748b7e81df52baa26caa
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-09-21 16:35:09 +0000

    l10n: Danish update

--- include/Comparers.h
+++ include/Comparers.h
@@ -35,4 +35,4 @@ template< typename TData > constexpr bool IsLess( const TData A, const TData B, 
 /**
  *  \brief Проверяемое число 0 или типа того
  */
-template< typename TData > constexpr bool 
+template< typename TData > constexpr bool isZero( const TData A, const double aAbsoluteZero = gAbsoluteZero  
