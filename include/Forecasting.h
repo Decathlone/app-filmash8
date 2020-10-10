196d8a1b9479fcca962493c572a39c9d70af54a2
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-10-10 10:43:44 +0000

    Create chat.lua

--- include/Forecasting.h
+++ include/Forecasting.h
@@ -2,4 +2,10 @@
  * File:   Forecasting.h
  * Author: kan <kansoftware.ru>
  *
- * Created on January 23, 2017, 
+ * Created on January 23, 2017, 1:58 PM
+ */
+
+#ifndef FORECASTING_H
+#define FORECASTING_H
+
+#include "CIn
