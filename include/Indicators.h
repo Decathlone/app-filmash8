398744beda6f30c17f2f7ce72c1fcc222b6c4035
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-10-22 06:00:16 +0000

    button

--- include/Indicators.h
+++ include/Indicators.h
@@ -1,4 +1,7 @@
 /**
  * \file Indicators.h
  * \brief Модуль реализующий индикаторы для одноногих стратегий
- * \author kan <kanso
+ * \author kan <kansoftware.ru>
+ * \since 2015-11-10
+ * \date 2019-07-31
+ * Модуль используе
