ba14f23fc1f4da3cc00c1160695dd67b2f40145e
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-10-23 19:31:55 +0000

    'resume'

--- include/Indicators.h
+++ include/Indicators.h
@@ -4,4 +4,7 @@
  * \author kan <kansoftware.ru>
  * \since 2015-11-10
  * \date 2019-07-31
- * Модуль используе
+ * Модуль используется неизменно в проектах TradeBot и [R] BackTester
+ */
+
+#ifndef
