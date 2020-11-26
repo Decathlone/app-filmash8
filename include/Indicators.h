6b876b259dde96e9c804fb8b353694cf92250de7
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-11-26 17:14:51 +0000

    Unmarked -dev.

--- include/Indicators.h
+++ include/Indicators.h
@@ -39,4 +39,5 @@ TPriceSeries _AbsoluteZigZag( const TBarSeries & aBars, const double aGap );
 TPriceSeries _MACD( const TPriceSeries & aPrices, const int aFastPeriod, const int aSlowPeriod, const int aSmoothPeriod );
 TPriceSeries _ZigZag( const TBarSeries & aBars, const double aGap );
 
-bool _Forecasting( const TPriceSeries & aPrices, const s
+bool _Forecasting( const TPriceSeries & aPrices, const size_t aForecastPeriod, const double aConfidenceIntervals, 
+            
