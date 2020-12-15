41d2c2b8baa86cca42f586f24dba090505347305
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-12-15 22:17:52 +0000

    Remove unused member variable.

--- include/PnlAction.h
+++ include/PnlAction.h
@@ -15,3 +15,56 @@
 #include <vector>
 #include <map>
 #include <string>
+
+#include "BasisOfStrategy.h"
+
+const double gMaxBedAttraction = -99.9;
+const double gFiltredByCount = -33.3;
+const double gFiltredByCountPnl = -66.6;
+const double gFiltredByPnl = 0.0;
+
+const size_t gMinDealsForAllTime = 6UL;
+const size_t gMinDealsStatictics = 30UL;
+const size_t gWellDealsStatictics = 1000UL;
+
+typedef std::map<std::string, double> TValues;
+
+struct TDeal {
+    TInnerDate OpenTime = 0.0;
+    TPrice OpenPrice = GetBadPrice();
+    TPrice StopLoss = GetBadPrice();
+    TPrice TakeProfit = GetBadPrice();
+    TPrice ClosePrice = GetBadPrice();
+    TInnerDate CloseTime = 0.0;
+    TDealSide DealSide = TDealSide::None;
+    size_t Volume = 0;
+    TValues Additive;
+
+    TDeal(){;}
+
+    TDeal(
+        const TInnerDate aOpenTime,
+        const TPrice aOpenPrice,
+        const TPrice aStopLoss,
+        const TPrice aTakeProfit,
+        const TPrice aClosePrice,
+        const TInnerDate aCloseTime,
+        const TDealSide aDealSide,
+        const size_t aVolume = 1,
+        const TValues aAdditive={} ) :
+        OpenTime( aOpenTime ),
+        OpenPrice( aOpenPrice ),
+        StopLoss( aStopLoss ),
+        TakeProfit( aTakeProfit ),
+        ClosePrice( aClosePrice ),
+        CloseTime( aCloseTime ),
+        DealSide( aDealSide ),
+        Volume( aVolume ),
+        Additive( aAdditive ){;}
+
+    void Reset() {
+        OpenTime = 0.0;
+        ResetPrice( OpenPrice );
+        ResetPrice( ClosePrice );
+        CloseTime = 0.0;
+        DealSide = TDealSide::None;
