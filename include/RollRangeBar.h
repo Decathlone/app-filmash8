ce3a54f18bbae84542d81289234171657c241b21
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-12-20 21:25:29 +0000

    Updated ignore

--- include/RollRangeBar.h
+++ include/RollRangeBar.h
@@ -24,3 +24,70 @@
 class TRollRangeBar {
     public:
         using TRange = std::pair<double, double>;//min max
+
+        TRollRangeBar( const size_t aN, const double aBadValue = -333333.33 ) :
+            n( aN ),
+            fBadValue( std::make_pair(aBadValue,aBadValue) ),
+            range( fBadValue),
+            fArrayReserve( std::max(aN, 1000UL) ) {
+            if (n < 1) throw std::invalid_argument("n must be greater than 0");
+
+            History.reserve( fArrayReserve );
+        }
+
+        void Add( const TSimpleBar& value ) {
+            window.push( std::make_pair(value.Low, value.High) );
+            windowSortedMin.insert(value.Low);
+            windowSortedMax.insert(value.High);
+
+            if( window.size() > n ){
+                windowSortedMin.erase( windowSortedMin.find(window.front().first) );
+                windowSortedMax.erase( windowSortedMax.find(window.front().second) );
+
+                window.pop();
+            }
+            
+            range = std::make_pair( *windowSortedMin.begin(), *windowSortedMax.rbegin() );
+
+            if( IsFormed() ){
+                History.push_back( range );
+            } else {
+                History.push_back( fBadValue );
+            }
+
+            fCached_m = false;
+            fCached_e = false;
+        }
+
+        bool IsFormed() const {
+            return window.size() == n;
+        }
+
+        TRange GetValue() const {
+            return range;
+        }
+
+        std::vector< TRange > GetHistory() const {
+            return History;
+        }
+
+        void Reset() {
+            window={};
+            windowSortedMax.clear();
+            windowSortedMin.clear();
+
+            History.clear();
+            History.reserve( fArrayReserve );
+
+            fCached_m = false;
+            fCached_e = false;
+        }
+
+        double getMean() const {
+            if( not IsFormed() ){
+                return NAN;
+            }
+            
+            if(fCached_m){
+                return fMean;
+            }
