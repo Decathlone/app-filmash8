7b597b18ba1c19672f524882d3647c51e2f58f21
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-12-21 20:35:36 +0000

    Removed unnecessary whitespace

---
+++ include/RollSMA.h
@@ -0,0 +1,56 @@
+
+/*** 
+ * File:   RollSMA.h
+ * Author: kan
+ *
+ * Created on November 13, 2019, 12:55 PM
+ * \date 2019-11-26
+ * The idea is from https://bitbucket.org/quanttools/quanttools/src/master/inst/include/Indicators/Sma.h
+ */
+
+#ifndef ROLLSMA_H
+#define ROLLSMA_H
+
+#include <queue>
+#include <stdexcept>
+#include <cmath>
+
+class RollSMA {
+    private:
+        double fSum;
+        const std::size_t fN;
+        std::queue< double > fWindow;
+
+    public:
+        RollSMA( const int n ) :
+            fSum( 0.0 ),
+            fN( static_cast<size_t>(n) ) {
+                if (n < 1) throw std::invalid_argument("n must be greater than 0");
+            }
+
+        void Add( double value ) {
+
+            fSum += value;
+            fWindow.push(value);
+
+            if (fWindow.size() > fN) {
+                fSum -= fWindow.front();
+                fWindow.pop();
+            }
+        }
+
+        bool IsFormed() const {
+            return fWindow.size() == fN;
+        }
+
+        double GetValue() const {
+            return ((fWindow.size() == fN) ? (fSum / static_cast<double>(fN)) : NAN);
+        }
+
+        void Reset() {
+            fSum = 0.0;
+            fWindow = {};
+        }
+};
+
+#endif /* ROLLSMA_H */
---
+++ include/RubberBuffer.h
@@ -0,0 +1,7 @@
+
+/*** 
+ * \file RubberBuffer.h
+ * \author kan <kansoftware.ru>
+ * \since 2020-09-16
+ * \date 2020-09-16
+ * \brief По безразмерному буферу рассчитывает min/max/Mean/err(TR)
