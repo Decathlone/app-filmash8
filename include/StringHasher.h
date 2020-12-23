982428870ef33a3298dbfa3b9018014c521c59a8
Decathlone <106426645+Decathlone@users.noreply.github.com>
2020-12-23 22:45:12 +0000

    Run update-remote.sh on ghost

--- include/StringHasher.h
+++ include/StringHasher.h
@@ -3,4 +3,8 @@
  * \brief Модуль содержащий функции для перевода чисел в хеш-строку
  * \author kan <kansoftware.ru>
  * \since 2015-08-25
- * \da
+ * \date 2015-08-25
+ */
+
+#ifndef STRINGHASHER_H
+#define	STRINGHASH
