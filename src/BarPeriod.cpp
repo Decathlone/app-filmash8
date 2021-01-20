10b01cac99edd2bd6175be88488b525d9f0fd860
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-01-20 05:50:02 +0000

    replace groovy script with ability in card script

--- src/BarPeriod.cpp
+++ src/BarPeriod.cpp
@@ -10,4 +10,5 @@
 const char *gBarPeriodNames[ static_cast<size_t>( TBarPeriod::maxValue ) ] = { "0MIN", "1MIN", "5MIN", "10MIN", "15MIN", "1HOUR", "4HOURS", "1DAY", "1WEEK", "1SEC" };
 
 //-----------------------------------------------------------------------------
-std:
+std::string getBarPeriodName( const TBarPeriod aID ) {
+    assert( aID > TBa
