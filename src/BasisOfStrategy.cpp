1ffed6d3055c6c6b13f6dff97148a4a139035a7f
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-01-24 17:12:29 +0000

    Rerun of benchmarks.

--- src/BasisOfStrategy.cpp
+++ src/BasisOfStrategy.cpp
@@ -44,3 +44,63 @@ bool IsOneHour( const TInnerDate aLeft, const TInnerDate aRight, const double aD
 
     return Trunc( aLeft / (gOneHour*aDuration) ) == Trunc( aRight / (gOneHour*aDuration) );
 }
+
+//------------------------------------------------------------------------------------------
+bool IsValidBar( const TSimpleBar & aBar ) {
+    return IsGreat( aBar.Volume, -2.0 * gAbsoluteZero ) // >= 0
+           and IsValidPrice( aBar.Open )
+           and IsValidPrice( aBar.High )
+           and IsValidPrice( aBar.Low )
+           and IsValidPrice( aBar.Close );
+}
+
+//------------------------------------------------------------------------------------------
+#define FAST_DECODER( aField ) \
+    for( std::size_t lRowNum = 0; lRowNum < aBar.size(); ++lRowNum ) { \
+        TSimpleBar lBar = aBar[ lRowNum ]; \
+        TPrice lPrice = lBar.aField; \
+        TSimpleTick lTick { lBar.DateTime, lPrice, lBar.Volume }; \
+        lResult[ lRowNum ] = lTick; \
+    } \
+// end of FAST_DECODER
+
+TPriceSeries BarsToPriceSeries( const TBarSeries &aBar, const TMAPoint aType ) {
+
+    TPriceSeries lResult( aBar.size() );
+
+    switch ( aType ) {
+        /////////
+        case TMAPoint::MAP_Close : {
+            FAST_DECODER( Close );
+        } break ;
+        /////////
+        case TMAPoint::MAP_Mid : {
+            for( std::size_t lRowNum = 0; lRowNum < aBar.size(); ++lRowNum ) {
+                TSimpleBar lBar = aBar[ lRowNum ];
+                TPrice lPrice = ( lBar.High + lBar.Low ) / 2.0 ;
+                TSimpleTick lTick { lBar.DateTime, lPrice, lBar.Volume };
+                lResult[ lRowNum ] = lTick;
+            }
+        } break ;
+        /////////
+        case TMAPoint::MAP_Triple : {
+            for( std::size_t lRowNum = 0; lRowNum < aBar.size(); ++lRowNum ) {
+                TSimpleBar lBar = aBar[ lRowNum ];
+                TPrice lPrice = ( lBar.High + lBar.Low + lBar.Close ) / 3.0 ;
+                TSimpleTick lTick { lBar.DateTime, lPrice, lBar.Volume };
+                lResult[ lRowNum ] = lTick;
+            }
+        } break ;
+        /////////
+        case TMAPoint::MAP_Open : {
+            FAST_DECODER( Open );
+        } break ;
+        /////////
+        case TMAPoint::MAP_High : {
+            FAST_DECODER( High );
+        } break ;
+        /////////
+        case TMAPoint::MAP_Low : {
+            FAST_DECODER( Low );
+        } break ;
+        /////////
