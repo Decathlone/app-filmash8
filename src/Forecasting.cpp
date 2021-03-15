0a0e625fdbbaa870f2a6e32dedff3e4aef232bcf
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-03-15 05:58:22 +0000

    net: Statically initialize init_net.dev_base_head

--- src/Forecasting.cpp
+++ src/Forecasting.cpp
@@ -1,2 +1,6 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* 
+ * File:   Forecasting.cpp
+ * Author: victorg(mq5)
