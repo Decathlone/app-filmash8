76df5db4ca58c43de19850147648acbdb857dd15
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-04-06 09:48:24 +0000

    Removed unused header

---
+++ src/Indicators.cpp
@@ -0,0 +1,7 @@
+
+/** 
+ * This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+ * PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+ * \file Indicators.cpp
+ * \author kan <kansoftware.ru>
+ * \since 2015-11-10
