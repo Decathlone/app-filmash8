4ed8172529cb5b1b37d65e4e6c717dd511b98369
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-04-07 09:09:24 +0000

    fill readme

--- src/Indicators.cpp
+++ src/Indicators.cpp
@@ -5,3 +5,60 @@
  * \file Indicators.cpp
  * \author kan <kansoftware.ru>
  * \since 2015-11-10
+ * \date 2020-10-21
+ */
+
+#include <cmath>
+#include <stdexcept>
+
+#include "DelphisRound.h"
+#include "Comparers.h"
+#include "Forecasting.h"
+#include "PnlAction.h"
+#include "RollRange.h"
+#include "RollRangeBar.h"
+
+#include "Indicators.h"
+
+//------------------------------------------------------------------------------------------
+TPriceSeries _TrueRange( const TBarSeries & aBars ) {
+
+    TPriceSeries lResult;
+    lResult.reserve( aBars.size() );
+
+    if( not aBars.empty() ) {
+
+        TSimpleBar lPriorBar( *aBars.begin() );
+        
+        lResult.emplace_back( 
+            lPriorBar.DateTime,
+            lPriorBar.High - lPriorBar.Low,
+            lPriorBar.Volume 
+        );
+
+        for( size_t i = 1; i < aBars.size(); ++i ) {
+            const TSimpleBar lCurrentBar( aBars[ i ] );
+            const TPrice lHigh = std::max( lPriorBar.Close , lCurrentBar.High );
+            const TPrice lLow = std::min( lPriorBar.Close , lCurrentBar.Low );
+
+            lResult.emplace_back(
+                lCurrentBar.DateTime,
+                lHigh - lLow,
+                lCurrentBar.Volume
+            );
+            lPriorBar = lCurrentBar;
+        }
+    }
+
+    return lResult;
+}
+
+//------------------------------------------------------------------------------------------
+TPriceSeries _AverageTrueRange( const TBarSeries & aBars, const int aPeriod ) {
+    const TPriceSeries lPrices( _TrueRange( aBars ) );
+    const TPriceSeries lTMPResult( _SmoothedMA( lPrices, aPeriod, 1 ) );
+    return lTMPResult;
+}
+
+//------------------------------------------------------------------------------------------
+TPriceSeries _SimpleMA( const TPriceSeries & aPrices, const int aPeriod, const size_t aLag ) {
