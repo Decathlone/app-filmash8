34d2ea6eaabbd6e34906591081309a57b6e3187a
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-05-03 20:10:49 +0000

    new binaries

--- src/PowellsMethod.cpp
+++ src/PowellsMethod.cpp
@@ -1,2 +1,6 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/** 
+ * File:   PowellsMethod.cpp
+ * Author: victorg(mq5), kan
