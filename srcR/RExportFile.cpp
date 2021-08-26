9f925d258290a2845b8bfb22c2f9381a4f0219ef
Decathlone <106426645+Decathlone@users.noreply.github.com>
2021-08-26 11:32:47 +0000

    finish merge

--- srcR/RExportFile.cpp
+++ srcR/RExportFile.cpp
@@ -19,4 +19,5 @@
 Rcpp::NumericVector SimpleMA( const Rcpp::NumericMatrix & aXts, const int aPeriod, const int aType ) {
     const TMAPoint lMAPoint = static_cast< TMAPoint >( aType );
     std::string lTZone;
-    const TPriceSeri
+    const TPriceSeries lPrices( XtsToPriceSeries( aXts, lMAPoint, lTZone ) );
+    
