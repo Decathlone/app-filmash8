7ffe6c393218e26ae35c86a47b0b8e87e3e066e5
Decathlone <106426645+Decathlone@users.noreply.github.com>
2022-06-13 16:02:04 +0000

    E-commerceProject

--- srcR/RExportFile.cpp
+++ srcR/RExportFile.cpp
@@ -298,4 +298,7 @@ Rcpp::List ForwardMinMax( const Rcpp::NumericMatrix & aOHLCV, const int aTimeDel
 //------------------------------------------------------------------------------------------
 Rcpp::NumericVector ChannelSize( const Rcpp::NumericMatrix & aOHLCV, const int aPeriod ) {
     const TBarSeries lBars( XtsToBarSeries( aOHLCV ) );
-    const TPriceSeries lResult( _ChannelSize( lBars, aPeriod )
+    const TPriceSeries lResult( _ChannelSize( lBars, aPeriod ) );
+    
+    const std::string lTZone(getTimeZone(aOHLCV));
+    return Pr
