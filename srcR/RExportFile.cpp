fce5eaa9dc1edf00f97ad8ea491c809fa021efc7
Decathlone <106426645+Decathlone@users.noreply.github.com>
2022-07-16 05:41:36 +0000

    added MIT license file

--- srcR/RExportFile.cpp
+++ srcR/RExportFile.cpp
@@ -305,4 +305,5 @@ Rcpp::NumericVector ChannelSize( const Rcpp::NumericMatrix & aOHLCV, const int a
 }
 
 //------------------------------------------------------------------------------------------
-Rcpp::NumericVector SAR( const Rcpp::NumericMatrix & aOHLCV, const 
+Rcpp::NumericVector SAR( const Rcpp::NumericMatrix & aOHLCV, const double aAccFactor, const double aMaxAccFactor, const bool aOvernight ) {
+   
