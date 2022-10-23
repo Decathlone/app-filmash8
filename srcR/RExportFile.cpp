a5adde373e93a3f5ee21fb23db389cc072dacdb5
Decathlone <106426645+Decathlone@users.noreply.github.com>
2022-10-23 14:39:43 +0000

    Test in progress

--- srcR/RExportFile.cpp
+++ srcR/RExportFile.cpp
@@ -380,4 +380,5 @@ Rcpp::NumericMatrix BollingerBands( const Rcpp::NumericMatrix & aXts, const int 
     }
     
     lIndex.attr("tzone") = aXts.attr("tzone");// "Europe/Moscow"; // the index has attributes
-    lIndex.attr("tclass") =
+    lIndex.attr("tclass") = "POSIXct";
+    lResult.attr("dim") = Rcpp::IntegerVector::crea
