fb77ba98a78638c01111c043c125e919891fcb09
Decathlone <106426645+Decathlone@users.noreply.github.com>
2022-11-23 13:52:01 +0000

    Merging new upstream

--- srcR/RExportFile.cpp
+++ srcR/RExportFile.cpp
@@ -402,4 +402,5 @@ Rcpp::NumericMatrix ConvertBars( const Rcpp::NumericMatrix & aXts, const int aPe
     if( aPeriod > static_cast< int >( TBarPeriod::minValue ) and 
         aPeriod < static_cast< int >( TBarPeriod::maxValue ) ) {
         
- 
+        const TBarSeries aOldBars = XtsToBarSeries( aXts );
+        lNe
